30482b03ea4962825cee6ef112356b23f35ff0c1
zhekai-zhao <zhekai1@ualberta.ca>
2021-11-01 13:48:06 -0600

    Add test

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -47,4 +47,12 @@ public class CustomList extends ArrayAdapter<City> {
         return view;
 
     }
+
+    public int getCount(){
+        return cities.size();
+    }
+
+    public void addCity(City city){
+        return;
+    }
 }
---
+++ app/src/test/java/TestListCity.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.simpleparadox.listycity.City;
+import com.example.simpleparadox.listycity.CustomList;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class TestListCity {
+
+    private CustomList list;
+
+    @BeforeAll
+    public void createList(){
+        list = new CustomList(null,new ArrayList<City>());
+    }
+
+    @Test
+    public void addCityTest(){
+        int listSize = list.getCount();
+        list.addCity(new City("halifax","NS"));
+        assertEquals(list.getCount(),listSize+1);
+    }
+}
